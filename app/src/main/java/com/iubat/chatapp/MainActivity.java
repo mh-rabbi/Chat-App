83dd77b1dd4226bd4c5d490ba181aff0b7d7c96b
mh-rabbi <mhr221official@gmail.com>
2025-05-01 21:20:24 +0600

    MessageModel, SendMessage fuctionaity added

--- app/src/main/java/com/iubat/chatapp/MainActivity.java
+++ app/src/main/java/com/iubat/chatapp/MainActivity.java
@@ -8,19 +8,26 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.annotations.Nullable;
 
 public class MainActivity extends AppCompatActivity {
 
     EditText edtName, edtMessage;
     TextView txtMessage;
     ImageView imgSend;
+    FirebaseDatabase db;
+    DatabaseReference databaseReference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,10 +47,35 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        FirebaseDatabase db = FirebaseDatabase.getInstance();
-        DatabaseReference databaseReference = db.getReference("message");
-        databaseReference.setValue("Hello World!");
+        db = FirebaseDatabase.getInstance();
+        databaseReference = db.getReference("message");
+       // databaseReference.setValue("Hello World!");
+        databaseReference.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
 
+            }
+
+            @Override
+            public void onChildChanged(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+
+            }
+
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot snapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
     }
 
     private void sendMessage() {
@@ -55,7 +87,11 @@ public class MainActivity extends AppCompatActivity {
             return;
         }
         if (!message.isEmpty()) {
-
+            edtName.setVisibility(View.GONE);
+            edtMessage.setText("");
+            DatabaseReference dr = databaseReference.push();
+            MessageModel messageModel = new MessageModel(dr.getKey(), name, message, System.currentTimeMillis());
+            dr.setValue(messageModel);
         }
     }
 }
--- app/src/main/java/com/iubat/chatapp/MessageModel.java
+++ app/src/main/java/com/iubat/chatapp/MessageModel.java
@@ -1,4 +1,34 @@
 package com.iubat.chatapp;
 
 public class MessageModel {
+    String id;
+    String name;
+    String message;
+    long time;
+
+    public MessageModel() {
+        // Required empty public constructor
+    }
+    public MessageModel(String id, String name, String message, long time) {
+        this.id = id;
+        this.name = name;
+        this.message = message;
+        this.time = time;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public long getTime() {
+        return time;
+    }
 }
